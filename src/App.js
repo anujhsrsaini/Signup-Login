d57a441f8cc53236a71d56d79bc9db1d1db4b369
anujhsrsaini <anujhsrsaini@gmail.com>
2023-12-26 11:26:53 +0530

    OTPLess testing

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  useEffect(() => {
+    window.otpless = (otplessUser) => {
+     alert(JSON.stringify(otplessUser));
+     console.log(JSON.stringify(otplessUser));
+    };
+   }, []);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div id="otpless-login-page">
+          <script type="text/javascript" src="https://otpless.com/auth.js" cid="MGMBEBXJXGO6J8T4BMHETORV64V724VE"></script>
+        </div>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
